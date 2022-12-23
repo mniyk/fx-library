d625815a9c8ef721a0a7033ff3275c65aea6663d
mniyk <my.name.is.yohei.kono@gmail.com>
2022-12-24 06:33:46 +0900

    Add: add file __init__.py in fx_library

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name='fx-library',
     description='FX related library with Pyrhon',
-    version='0.0.1',
+    version='0.0.2',
     author='mniyk',
     author_email='my.name.is.yohei.kono@gmail.com',
     url='https://github.com/mniyk/fx-library.git',
