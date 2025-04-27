b6509b7dbd8259739ca6d66bea6178d3836dac8f
mniyk <my.name.is.yohei.kono@gmail.com>
2025-04-27 17:54:33 +0900

    Update: technical_indicators.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name='fx-library',
     description='FX related library with Pyrhon',
-    version='6.2.0',
+    version='6.3.0',
     author='mniyk',
     author_email='my.name.is.yohei.kono@gmail.com',
     url='https://github.com/mniyk/fx-library.git',
